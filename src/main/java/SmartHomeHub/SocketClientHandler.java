5509023eb72117672600011ed955b65c385947db
Giacomo Tommaso Petrucci <giacomo.tom.petrucci@tiscali.it>
2019-07-03 18:44:40 +0200

    Prima stesura di SocketClientHandler

--- src/main/java/SmartHomeHub/SocketClientHandler.java
+++ src/main/java/SmartHomeHub/SocketClientHandler.java
@@ -1,18 +1,25 @@
 package SmartHomeHub;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class SocketClientHandler implements Runnable {
     private Socket clientSocket;
     private BufferedReader in;
+    private BufferedWriter writer;
 
     public SocketClientHandler(Socket clientSocket) {
         this.clientSocket=clientSocket;
         try {
-            BufferedReader in = new BufferedReader(
+            this.in = new BufferedReader(
                     new InputStreamReader(clientSocket.getInputStream()));
+            this.writer= Files.newBufferedWriter(Paths.get("test"), Charset.forName("ASCII"));
         }
         catch(java.io.IOException e){
             System.err.println(e.getMessage());
@@ -21,6 +28,14 @@ public class SocketClientHandler implements Runnable {
 
     @Override
     public void run() {
+        while(true){
 
+            try {
+                if (in.ready()) writer.write(in.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
